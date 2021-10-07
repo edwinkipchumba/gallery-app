f7d4abdc898bdfa890f881a66b137af213e132c3
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-07 22:31:26 +0300

    add request to return responses to the user

--- picts/views.py
+++ picts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Image, Location
 
 # Create your views here.
+def index(request):
+    images = Image.objects.all()
+    locations = Location.get_locations()
+    print(locations)
+    return render(request, 'pictures/index.html', {'images': images[::-1], 'locations': locations})
