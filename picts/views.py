f0ebf7df40ce0626de437db09465695bb308e347
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-07 22:33:49 +0300

    adding request location

--- picts/views.py
+++ picts/views.py
@@ -7,3 +7,9 @@ def index(request):
     locations = Location.get_locations()
     print(locations)
     return render(request, 'pictures/index.html', {'images': images[::-1], 'locations': locations})
+
+# location request image
+def image_location(request, location):
+    images = Image.filter_by_location(location)
+    print(images)
+    return render(request, 'pictures/location.html', {'location_images': images})
