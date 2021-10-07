2a78c1aeb1eb4284399502def9ff0d29f71c2853
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-07 22:36:50 +0300

    adding function search-image

--- picts/views.py
+++ picts/views.py
@@ -13,3 +13,15 @@ def image_location(request, location):
     images = Image.filter_by_location(location)
     print(images)
     return render(request, 'pictures/location.html', {'location_images': images})
+
+# search results
+def search_results(request):
+    if 'imagesearch' in request.GET and request.GET["imagesearch"]:
+        category = request.GET.get("imagesearch")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        print(searched_images)
+        return render(request, 'pictures/search_results.html', {"message": message, "images": searched_images})
+    else:
+        message = "You haven't searched for any image category"
+        return render(request, 'pictures/search_results.html', {"message": message})
