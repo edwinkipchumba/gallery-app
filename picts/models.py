e09876bdb6709559def159e85f96ae94e320ec5d
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-08 12:17:51 +0300

    adding image class

--- picts/models.py
+++ picts/models.py
@@ -13,6 +13,7 @@ class Category(models.Model):
     def delete_category(self):
         self.delete()
         
+        
     # class location
 class Location(models.Model):
     name = models.CharField(max_length=60)
@@ -34,3 +35,45 @@ class Location(models.Model):
 
     def delete_location(self):
         self.delete()
+        
+        
+    # class images
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    author = models.CharField(max_length=40, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+
+    @classmethod
+    def filter_by_location(cls, location):
+        image_location = Image.objects.filter(location__name=location).all()
+        return image_location
+
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    @classmethod
+    def search_by_category(cls, category):
+        images = cls.objects.filter(category__name__icontains=category)
+        return images
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    class Meta:
+        ordering = ['date']
