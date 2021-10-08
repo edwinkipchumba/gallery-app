9b1e02090ea3bc4439cc5a241b12706c878ec572
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-08 08:30:34 +0300

    adding setup method to display location

--- picts/tests.py
+++ picts/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Image, Category, Location
 
 # Create your tests here.
+class TestImage(TestCase):
+    
+    def setUp(self):
+        self.location = Location(name='Moringa')
+        self.location.save_location()
+
+        self.category = Category(name='home')
+        self.category.save_category()
+
+        self.image_test = Image(id=1, name='image', description='this is a test image', location=self.location,
+                                category=self.category)
