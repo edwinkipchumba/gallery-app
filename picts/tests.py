09ca07052d3563bf3a63c7a0c0bb4661d2fe55ef
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-08 11:06:12 +0300

    add save method to save images in db

--- picts/tests.py
+++ picts/tests.py
@@ -19,3 +19,9 @@ class TestImage(TestCase):
         # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.image_test, Image))
+        
+        # testing save method
+    def test_save_image(self):
+        self.image_test.save_image()
+        after = Image.objects.all()
+        self.assertTrue(len(after) > 0)
