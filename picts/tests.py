19dffffd4ca1b48397bf748b8d77ea7aa499dbf3
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-08 10:57:36 +0300

    add test to confirm object is correctly instantiated

--- picts/tests.py
+++ picts/tests.py
@@ -4,6 +4,7 @@ from .models import Image, Category, Location
 # Create your tests here.
 class TestImage(TestCase):
     
+    # set up method
     def setUp(self):
         self.location = Location(name='Moringa')
         self.location.save_location()
@@ -13,3 +14,8 @@ class TestImage(TestCase):
 
         self.image_test = Image(id=1, name='image', description='this is a test image', location=self.location,
                                 category=self.category)
+        
+     
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, Image))
