45772c08c8de7bdb1a7dd666749cb6055dee5e2b
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-08 11:09:56 +0300

    add delete function to delete instances models in database

--- picts/tests.py
+++ picts/tests.py
@@ -25,3 +25,8 @@ class TestImage(TestCase):
         self.image_test.save_image()
         after = Image.objects.all()
         self.assertTrue(len(after) > 0)
+        
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
