49d28c96be8d153a595adc9a70d0b9b6e4fd92cd
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-09 10:55:26 +0300

    config cloudinary settings

--- pictures/settings.py
+++ pictures/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +131,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# setting configuration parameters globally
+cloudinary.config( 
+  cloud_name = "moringa-dev", 
+  api_key = "634372948129142", 
+  api_secret = "De--YnRdHaaFQ1tlZx6DwYChcZg",
+  
+)
