67cdbd088160487e412f3d3e8349bc6184185ef4
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-09 23:02:00 +0300

    update home page

--- picts/admin.py
+++ picts/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Image, Location, categories
+from .models import *
 # Register your models here.
 
 
 admin.site.register(Image)
 admin.site.register(Location)
-admin.site.register(categories)
+admin.site.register(Category)
